5fa1bc2c0ad2c59dcc8fd6aad9407f4da2df4fb4
Zulnabil <zulnabil.22@gmail.com>
2022-06-08 05:36:00 +0700

    feat: add swr config into root app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { SWRConfig } from "swr"
+
 import Dashboard from "pages/dashboard/dashboard.page"
 
 import "./common/styles/index.scss"
+import fetcher from "common/libs/fetch.lib"
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/analytic" element={<Dashboard />}></Route>
-        <Route path="/setting" element={<Dashboard />}></Route>
-        <Route path="/" element={<Dashboard />}></Route>
-      </Routes>
+      <SWRConfig
+        value={{
+          refreshInterval: 3000,
+          fetcher,
+        }}
+      >
+        <Routes>
+          <Route path="/analytic" element={<Dashboard />}></Route>
+          <Route path="/setting" element={<Dashboard />}></Route>
+          <Route path="/" element={<Dashboard />}></Route>
+        </Routes>
+      </SWRConfig>
     </Router>
   )
 }
