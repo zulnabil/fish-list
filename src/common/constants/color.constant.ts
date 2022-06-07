302de93ed902f3ca81dc9fe54eff1af07ca3dcd1
Zulnabil <zulnabil.22@gmail.com>
2022-06-07 20:53:02 +0700

    feat(color): add theme color constant

---
+++ src/common/constants/color.constant.ts
@@ -0,0 +1,12 @@
+const THEME_COLORS = {
+  primary100: "#51c8a2",
+  primary200: "#1c939b",
+  primary300: "#038767",
+  primary400: "#02513e",
+  secondary: "#ffd200",
+  blackDark: "#1d1d1d",
+  blackSoft: "#1f272e",
+  greyLight: "#ebeef0",
+}
+
+export default THEME_COLORS
---
+++ src/common/types/common.type.ts
@@ -0,0 +1,18 @@
+import THEME_COLORS from "common/constants/color.constant"
+
+export type ColorType = keyof typeof THEME_COLORS
+
+export interface ColorInterface {
+  color?: ColorType | string
+}
+
+export type DefaultThemeType =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "warning"
+  | "danger"
+  | "info"
+  | "text"
+
+export type DefaultSizeType = "large" | "default" | "small"
