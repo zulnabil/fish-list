b15a65359c3ab140ec822e1b2e11efc66b4319d3
Zulnabil <zulnabil.22@gmail.com>
2022-06-10 15:48:33 +0700

    feat(card): add className as prop

--- src/common/components/card/card.component.tsx
+++ src/common/components/card/card.component.tsx
@@ -1,4 +1,5 @@
 import { CardProps } from "common/components/card/types/card.type"
+import { objToString } from "common/helper/string.helper"
 import { FC } from "react"
 
 import "./styles/card.style.scss"
@@ -8,8 +9,15 @@ import "./styles/card.style.scss"
  * @param {CardProps} props
  * @returns {FC}
  */
-const CardComponent: FC<CardProps> = ({ children }) => {
-  return <div className="ui-card">{children}</div>
+const CardComponent: FC<CardProps> = ({
+  children,
+  className: classNameInitial,
+}) => {
+  const className = {
+    "ui-card": true,
+    [classNameInitial as string]: Boolean(classNameInitial),
+  }
+  return <div className={objToString(className)}>{children}</div>
 }
 
 export default CardComponent
