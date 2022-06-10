969b2a680dff8530cd827c9c00fba071f6fa6d46
Zulnabil <zulnabil.22@gmail.com>
2022-06-10 11:21:26 +0700

    feat(fetcher): use axios as fetcher instead

--- src/common/libs/fetch.lib.ts
+++ src/common/libs/fetch.lib.ts
@@ -1,4 +1,12 @@
-const fetcher = (arg: any, ...args: any) =>
-  fetch(arg, ...args).then((res) => res.json())
+import axios from "axios"
+import { BASE_STEIN_URL } from "common/config/env"
+
+const options = {
+  baseURL: BASE_STEIN_URL,
+}
+
+const instance = axios.create(options)
+
+const fetcher = (url: any) => instance.get(url).then((res) => res.data)
 
 export default fetcher
