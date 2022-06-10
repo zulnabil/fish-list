843c015d804d8579c1734e7b84c2c289f8665e28
Zulnabil <zulnabil.22@gmail.com>
2022-06-10 15:52:27 +0700

    fix(const): remove default offset due to data sync

--- src/modules/fish-list/constants/fish-list.constant.ts
+++ src/modules/fish-list/constants/fish-list.constant.ts
@@ -1,8 +1,8 @@
 export const DEFAULT_LIMIT = 15
-export const DEFAULT_OFFSET = 130 // start with cleaner data
-export const TOTAL_ITEMS = 270 // total items in the sheet
+export const DEFAULT_OFFSET = 0
+export const TOTAL_ITEMS = 290 // total items in the sheet
 export const TOTAL_PAGE = Math.floor(
-  (TOTAL_ITEMS - DEFAULT_OFFSET) / DEFAULT_LIMIT
+  (TOTAL_ITEMS - Math.max(DEFAULT_OFFSET, 0)) / DEFAULT_LIMIT
 )
 
 export const COLUMNS = [
